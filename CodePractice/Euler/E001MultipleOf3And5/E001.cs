38e4e876380f1f0de74a2f71fe3b7cec7d6ef41a
Nin <5350772+quellatalo@users.noreply.github.com>
2024-10-09 00:22:05 +0900

    add repo readme and code examples

---
+++ CodePractice/Euler/E001MultipleOf3And5/E001.cs
@@ -0,0 +1,36 @@
+﻿using System.Globalization;
+using System.Text;
+using Computing.Extensions;
+
+namespace CodePractice.Euler.E001MultipleOf3And5;
+
+class E001 : ISolution
+{
+    public const int MaxN = 1_000_000_000;
+    public const int MaxT = 100_000;
+    public const int Min = 1;
+
+    public static long MultipleOf3Or5(int upToExclusive)
+    {
+        int largest = upToExclusive - 1;
+        return 3L.SumOfArithmeticSeries(3, largest / 3) +
+               5L.SumOfArithmeticSeries(5, largest / 5) -
+               15L.SumOfArithmeticSeries(15, largest / 15);
+    }
+
+    public void Solve()
+    {
+        StringBuilder results = new();
+
+        // up to 100,000
+        int t = Convert.ToInt32(Console.ReadLine(), CultureInfo.InvariantCulture);
+        for (int i = 0; i < t; i++)
+        {
+            // up to 1,000,000,000
+            int n = Convert.ToInt32(Console.ReadLine(), CultureInfo.InvariantCulture);
+            results.AppendLine(MultipleOf3Or5(n).ToString(CultureInfo.InvariantCulture));
+        }
+
+        Console.WriteLine(results.ToString());
+    }
+}
---
+++ Computing/Extensions/ArithmeticExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Numerics;
+
+namespace Computing.Extensions;
+
+/// <summary>
+/// Common computations.
+/// </summary>
+public static class ArithmeticExtensions
+{
+    /// <summary>
+    /// Calculates the sum of an arithmetic sequence.
+    /// </summary>
+    /// <param name="firstNumber">The first term.</param>
+    /// <param name="difference">The common difference.</param>
+    /// <param name="numberOfSeries">The number of series.</param>
+    /// <typeparam name="T">Number type.</typeparam>
+    /// <returns>The sum of the series.</returns>
+    /// <remarks>https://www.mathsisfun.com/algebra/sequences-sums-arithmetic.html.</remarks>
+    public static T SumOfArithmeticSeries<T>(this T firstNumber, T difference, T numberOfSeries)
+        where T : IBinaryInteger<T>
+        => (numberOfSeries * ((firstNumber << 1) + ((numberOfSeries - T.One) * difference))) >> 1;
+}
