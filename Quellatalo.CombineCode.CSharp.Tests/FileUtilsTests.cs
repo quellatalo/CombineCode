a8cd0684f519015fce37a578fbd88e7237c3f80f
Nin <5350772+quellatalo@users.noreply.github.com>
2024-10-17 19:08:02 +0900

    try TUnit

---
+++ Quellatalo.CombineCode.CSharp.Tests/FileUtilsTests.cs
@@ -0,0 +1,21 @@
+﻿namespace Quellatalo.CombineCode.CSharp.Tests;
+
+public class FileUtilsTests
+{
+    [Test]
+    public async Task FindFromParentDirectories_SolutionFile_Success()
+    {
+        var searchResult = FileUtils.FindFromParentDirectories("*.sln");
+        using var multipleAssertion = Assert.Multiple();
+        await Assert.That(searchResult).IsNotNull();
+        await Assert.That(searchResult!.Length).IsEqualTo(1);
+        await Assert.That(searchResult[0].Name).IsEqualTo("CombineCode.sln");
+    }
+
+    [Test]
+    public async Task FindFromParentDirectories_NotExist_NotFound()
+    {
+        var searchResult = FileUtils.FindFromParentDirectories(Guid.NewGuid().ToString());
+        await Assert.That(searchResult).IsNull();
+    }
+}
